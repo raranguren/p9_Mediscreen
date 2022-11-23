ca60bcc6803afa9b3202fe57e535c81cd467227e
raranguren <ricaragas@hotmail.com>
2022-11-23 01:24:22 +0100

    new springboot api serving in port 8082

---
+++ api-notes/src/main/java/com/mediscreen/abernathy/notes/ApiNotesApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.abernathy.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiNotesApplication.class, args);
+    }
+
+}
---
+++ api-notes/src/test/java/com/mediscreen/abernathy/notes/ApiNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.abernathy.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
